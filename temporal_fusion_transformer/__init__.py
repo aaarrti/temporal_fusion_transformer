4e098c3dd061a1b5b23ff2bbd8a8e3fe936ab7d3
artem-sereda <artem.sereda@campus.tu-berlin.de>
2023-09-17 23:20:55 +0200

    :construction:

--- temporal_fusion_transformer/__init__.py
+++ temporal_fusion_transformer/__init__.py
@@ -1,10 +1,5 @@
 from typing import TYPE_CHECKING
 
-import jax
-
-jax.config.update("jax_softmax_custom_jvp", True)
-
-
 if TYPE_CHECKING:
     from temporal_fusion_transformer.src.config_dict import ConfigDict
 
